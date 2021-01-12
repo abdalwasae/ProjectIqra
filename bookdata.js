277336d78ad9c82f748f7c77511688bb3d8970ce
Mohammed Abdul Wasay <76831526+abdalwasae@users.noreply.github.com>
2021-01-12 23:29:17 +0530

    Add files via upload

---
+++ bookdata.js
@@ -0,0 +1,307 @@
+var data = {
+  "0": {
+    0: {
+      "title": "Educated: A Memoir",
+      "author": "By Tara Westover",
+      "rating": "⭐⭐⭐⭐",
+      "price": "₹ 2000",
+      "description": "1 NEW YORK TIMES, WALL STREET JOURNAL, AND BOSTON GLOBE BESTSELLER One of the most acclaimed books of our time",
+      "img": 'images/book1.jpeg'
+    },
+    1: {
+      "title": "We Want to Do More Than Survive: Abolitionist Teaching and the Pursuit of Educational Freedom",
+      "author": "By Tara WestoverBy Bettina Love",
+      "rating": "⭐⭐⭐⭐",
+      "price": "₹ 1453",
+      "description": "Winner of the 2020 Society of Professors of Education Outstanding Book Award Drawing on personal stories, research",
+      "img": 'images/book2.jpeg'
+    },
+    2: {
+      "title": "The Privileged Poor: How Elite Colleges Are Failing Disadvantaged Students Illustrated Edition",
+      "author": "By Anthony Abraham Jack",
+      "rating": "⭐⭐⭐⭐",
+      "price": "₹ 800",
+      "description": "An NPR Favorite Book of the Year Winner of the Critics’ Choice Book Award, American Educational Studies Association",
+      "img": 'images/book3.jpg'
+    },
+    3: {
+      "title": "Choosing College: How to Make Better Learning Decisions Throughout Your Life 1st Edition",
+      "author": "By Michael B.Horn",
+      "rating": "⭐⭐⭐⭐",
+      "price": "₹ 999",
+      "description": "This book is about addressing the college-choosing problem. The rankings, metrics, analytics, college visits",
+      "img": 'images/book4.jpeg'
+    },
+    4: {
+      "title": "For White Folks Who Teach in the Hood... and the Rest of Y'all Too: Reality Pedagogy and Urban Education (Race, Education, and Democracy)",
+      "author": "By Chirstopher Emdin",
+      "rating": "⭐⭐⭐⭐",
+      "price": "₹ 2500",
+      "description": "A New York Times Best Seller Merging real stories with theory, research, and practice, a prominent scholar offers",
+      "img": 'images/book5.jpeg'
+    },
+    5: {
+      "title": "Culturize: Every Student. Every Day. Whatever It Takes",
+      "author": "By Jimmy Casas",
+      "rating": "⭐⭐⭐⭐",
+      "price": "₹ 1800",
+      "description": "Eradicate Average! CULTURIZE Your School. Average schools don’t inspire greatness—and greatness is what our world needs.",
+      "img": 'images/book6.jpeg'
+    },
+    6: {
+      "title": "Powerful Teaching: Unleash the Science of Learning 1st Edition",
+      "author": "By Pooja K Agarwal",
+      "rating": "⭐⭐⭐⭐",
+      "price": "₹ 2750",
+      "description": "Powerful Teaching: Unleash the Science of Learning empowers educators to harness rigorous research on how students learn",
+      "img": 'images/book7.jpeg'
+    },
+    7: {
+      "title": "The Knowledge Gap: The hidden cause of America's broken education system--and how to fix it",
+      "author": "By Natalie Wexler",
+      "rating": "⭐⭐⭐⭐",
+      "price": "₹ 390",
+      "description": "The untold story of the root cause of America's education crisis--and the seemingly endless cycle of multigenerational poverty.",
+      "img": 'images/book8.jpeg'
+    }
+  },
+  "1":{
+    0: {
+        "title": "Vikramaditya Veergatha Book 4 - The Wrath of the Hellfires",
+        "author": "By  Sathrujeet Nath",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.294",
+        "description": "1 Shukracharya’s plan to break the unity of Vikramaditya’s Council has borne bitter fruit. Friends have become sworn enemies, and brother has turned against brother, setting Avanti on the path to self-destruction.",     
+        "img": 'images/fbook1.jpg'
+      },
+      1: {
+        "title": "The Girl Who Drank the Moon",
+        "author": "By  Kelly Barnhill",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.475",
+        "description": "THE NO 1 NEW YORK TIMES BESTSELLER AND NEWBERY MEDAL WINNER 'This beautifully written, darkly funny coming-of-age story will enchant and entertain' Daily Mail Every year",     
+        "img": 'images/fbook3.jpeg'
+      },
+      2: {
+        "title": "Chaturanga (Baahubali: Before the Beginning) Veergatha Book 4 - The Wrath of the Hellfires: A Memoir",
+        "author": "By  Sathrujeet Nath",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.396",
+        "description": "Shukracharya’s plan to break the Political intrigue is astir in the land of Mahishmathi.",     
+        "img": 'images/fbook2.jpg'
+      },
+      3: {
+        "title": "The Book Thief: The life-affirming number one international bestseller",
+        "author": "By  Sathrujeet Nath",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.587",
+        "description": "Life affirming, triumphant and tragic . . . masterfully told. . . but also a wonderful page-turner", 
+        "img": 'images/fbook4.jpg'
+      },
+      4:{
+        "title": "The Combat of Magic and Miracles",
+        "author": "By  Samuel Dharmendar",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.966",
+        "description": "Since time immemorial, wars between the mighty birds and the venomous reptiles were inevitable.", 
+        "img": 'images/fbook5.jpeg'
+      },
+      5:{
+        "title": "A Wrinkle in Time",
+        "author": "By  Madeleine L'Engle",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.325",
+        "description": "A WRINKLE IN TIME is a classic sci-fi adventure for children by bestselling US author, Madeleine L'Engle and is now major new motion picture from Disney films.", 
+        "img": 'images/fbook6.jpeg'
+      },
+      6: {
+        "title": "The Name of the Wind: The Kingkiller Chronicle: Book 1",
+        "author": "By  Patrick Rothfuss",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.788",
+        "description": "I was reminded of Ursula K. Le Guin, George R. R. Martin, and J. R. R. Tolkein, but never felt that Rothfuss was imitating anyone:THE TIMES", 
+        "img": 'images/fbook7.jpeg'
+      },
+      7:{
+        "title": "The Snowman of Nalanda",
+        "author": "By  Roshan Singh",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.199",
+        "description": "The book runs on three themes: the fall of Nalanda monastery, Yetis and the ongoing loss of nature. There are wizards, witches and yaksha (nature spirits)", 
+        "img": 'images/fbook8.jpg'
+      },
+      8: {
+        "title": "Red Queen",
+        "author": "By  Victoria Aveyard",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.355",
+        "description": "This is a world divided by blood - red or silver.The Reds are commoners, ruled by a Silver elite in possession of god-like superpowers.", 
+        "img": 'images/fbook9.jpg'
+      }
+  },
+  "2":{
+    0: {
+        "title": "Thanos Rising (Marvel Now)",
+        "author": "By  Jason Aaron",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.924",
+        "description": "The vile face of Thanos left audiences in shock after last summer's Marvel Studios' The Avengers movie...but who is this eerily disturbing villain?",
+        "img": 'images/cbook1.jpg'
+      },
+      1: {
+        "title": "Vikram and Betaal",
+        "author": "By Rungeen Singh",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.80",
+        "description": "Classic Hindi Tales of Vikram & Betal",
+        "img": 'images/cbook2.jpg'
+      },
+      2: {
+        "title": "Justice League 2016",
+        "author": "By David Turner",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.219",
+        "description": "Justice League 2016 DC Comic Books Part 1",
+        "img": 'images/cbook3.jpg'
+      },
+      3: {
+        "title": "The Best of Tenali Raman",
+        "author": "By Rungeen Singh ",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.294",
+        "description": "The Best of Tenali Raman by Mrs Rungeen Singh is a great compilation of interesting and entertaining stories of Tenali Raman right from his childhood to adulthood",
+        "img": 'images/cbook4.jpg'
+      },
+      4:{
+        "title": "Alice in the Wonderland - Story Book",
+        "author": "By Pegasus Team ",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.294",
+        "description": "On a lazy afternoon, Alice follows a talking white rabbit and falls into a huge rabbit hole! Alice finds herself in a strange land with bizarre characters and many weird things going on!",
+        "img": 'images/cbook5.jpg'
+      },
+      5:{
+        "title": "TINKLE DOUBLE DIGEST 1",
+        "author": "By Anant Pai",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.80",
+        "description": "Everyone’s favourite Shikari Shambu is running in circles with a man-eating tiger! Who will trump over whom? Find out in this episode of Shikari Shambu!",
+        "img": 'images/cbook6.jpg'
+      },
+      6: {
+        "title": "The Illustrated Stories Of Akbar and Birbal: Classic Tales From India",
+        "author": "By Wonder House Books",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.150",
+        "description": "The stories of ‘Akbar and Birbal’ are the most cherished folklores of India that have been passed from one generation to another, enthralling both young and adult readers.",
+        "img": 'images/cbook7.jpg'
+      },
+      7:{
+        "title": "Username: Evie",
+        "author": "By Joe Sugg",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.398",
+        "description": "The first book by YouTube star Joe Sugg tells the story of Evie, a socially-isolated teenage girl who struggles to fit in at high school.",
+        "img": 'images/cbook8.jpg'
+      },
+      8: {
+        "title": "Spider-Man: Homecoming Mega Movie Storybook",
+        "author": "By Marvel",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.394",
+        "description": "After the events of Marvel's Captain America: Civil War, Peter Parker returns home to Queens, balancing his normal life as a high school sophomore with fighting street crime.",
+        "img": 'images/fbook3.jpeg'
+      }
+  },
+  "3":{
+    0: {
+        "title": "Dark Matter: The Most Mind-Blowing And Twisted Thriller Of The Year",
+        "author": "By  Blake Crouch",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.365",
+        "description": "Brilliant. . . I think Blake Crouch just invented something new - Lee Child, author of the Jack Reacher series",
+        "img": 'images/sbook1.jpg'
+      },
+      1: {
+        "title": "Beyond Infinity",
+        "author": "By  Akash Siddharth",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.299",
+        "description": "When the world changed once and for all... In 2043, aliens arrived in huge numbers and attacked Earth. A war was waged and millions died but not in vain.",
+        "img": 'images/sbook2.jpg'
+      },
+      2: {
+        "title": "The Time Machine",
+        "author": "By  H.G Wells",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.85",
+        "description": "A compelling science fiction, the Time Machine is a first-hand account of a Time Traveler's journey into the future. a pull of the lever and the machine sends him to the year 802,701.",
+        "img": 'images/sbook3.jpg'
+      },
+      3: {
+        "title": "The Theory of Everything",
+        "author": "By  Stephen Hawking",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.149",
+        "description": "Seven lectures by the brilliant theoretical physicist have been compiled into this book to try to explain to the common man",
+        "img": 'images/sbook4.jpg'
+      },
+      4:{
+        "title": "The Martian: Stranded on Mars, one astronaut fights to survive",
+        "author": "By  Andy Wier",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.315",
+        "description": "A survival story for the 21st century and the international bestseller behind the major film from Ridley Scott starring Matt Damon and Jessica Chastain.",
+        "img": 'images/sbook5.jpg'
+      },
+      5:{
+        "title": "THE RISING SUN",
+        "author": "By  Samarth Behl",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.160",
+        "description": "A team of scientists work on a life-changing project. But it rather changed their lives in a way they never imagined as some serious crimes knock at thier door.",
+        "img": 'images/sbook6.jpg'
+      },
+      6: {
+        "title": "The Veiled Universe : Cosmic Tales of Science Fiction",
+        "author": "By  Rob Garnet",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.170",
+        "description": "What if we could travel at the speed of thought? Adrian did and he reached the edge of the universe and beyond.",
+        "img": 'images/sbook7.jpg'
+      },
+      7:{
+        "title": "Europa - A Sci-Fi Adventure Of Galactical Proportions",
+        "author": "By  Hywel Pinto",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.200",
+        "description": "The year is 2250 and we are witness to a historical trial of one of the Silver Fleet's youngest and most promising Captain",
+        "img": 'images/sbook8.jpg'
+      },
+      8: {
+        "title": "1984",
+        "author": "By  George Orwell",
+        "rating": "⭐⭐⭐⭐",
+        "price": "₹.153",
+        "description": "Written in 1948, 1984 was George Orwell’s chilling prophecy about the future. And while 1984 has come and gone",
+        "img": 'images/sbook9.jpg'
+      }
+  }
+}
+function next(){
+    console.log('hehjeh')
+    var search_keyword = document.getElementById('search_bar').value;
+    if(search_keyword == 'Educational books'){
+      window.location.href = 'educational.html'
+    }
+    if(search_keyword == 'Fantasy books'){
+      window.location.href = 'fantasy.html'
+    }
+    if(search_keyword == 'Comic books'){
+      window.location.href = 'comic.html'
+    }
+    if(search_keyword == 'Science Fiction Books'){
+      window.location.href = 'sciencefiction.html'
+    }
+  }
+  var user=''
